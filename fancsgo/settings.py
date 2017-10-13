73e82d8feb19c4a6b5d4f8310a40f412fd555dcd
hfwu <376276511@qq.com>
2017-10-13 09:52:00 +0800

    fiex drft

--- fancsgo/settings.py
+++ fancsgo/settings.py
@@ -218,6 +218,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
     ),
